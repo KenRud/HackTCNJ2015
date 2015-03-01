64516498df4d916569b1bc62bd938396ca9074ad
Steven Kolln <stee11ftw@gmail.com>
2015-03-01 00:59:47 -0500

    Final database hopefully?

--- back-end/app.py
+++ back-end/app.py
@@ -33,41 +33,48 @@ def create_account():
 	conn = connect_db()
 	db = conn.cursor()
 	db.execute("PRAGMA foreign_keys = ON;")
+	db.execute("SELECT * FROM USER WHERE NAME = ?", (user,))
+	result = db.fetchall()
+	return "0"
+	if result is not []:
+		return "Error"
 	db.execute("INSERT INTO USER (NAME,PASSWORD) VALUES (?,?);", (user, password))
 	conn.commit()
 	conn.close()
-	return "0"
+	return "Success"
+
+@app.route("/login", methods = ["POST", "GET"])
+def login():
+	incoming = request.json
+	user = incoming['name']
+	password = incoming['password']
+	conn = connect_db()
+	db = conn.xcursor()
+	db.execute("PRAGMA foreign_keys = ON;")
+	db.execute("SELECT * FROM USER WHERE NAME = ? AND PASSWORD = ?", (user, password))
+	result = db.fetchone()
+	conn.commit()
+	conn.close()
+	if result is None:
+		return "Error"
+	else:
+		return "Success"
 
 @app.route("/make_friends", methods = ["POST", "GET"])
 def make_friends():
 	incoming = request.json
 	user1 = incoming['name1']
 	user2 = incoming['name2']
+	both_users = [user1, user2]
+	both_users.sort()
+	user1,user2 = both_users[0],both_users[1]
 	conn = connect_db()
 	db = conn.cursor()
 	db.execute("PRAGMA foreign_keys = ON;")
-	db.execute("SELECT * FROM FRIENDS WHERE NAME1 = ? AND NAME2 = ?", (user1, user2))
-	first_query = db.fetchone()
-	db.execute("SELECT * FROM FRIENDS WHERE NAME1 = ? AND NAME2 = ?", (user2, user1))
-	second_query = db.fetchone()
-	print first_query,second_query
-	if first_query is not None or second_query is not None:
-		return "Frienship is already made."
 	db.execute("INSERT INTO FRIENDS (NAME1,NAME2) VALUES (?,?);", (user1, user2))
 	conn.commit()
 	conn.close()
-	return "0"
-
-@app.route("/return_games", methods = ["POST", "GET"])
-def return_games():
-	incoming = request.json
-	user = incoming['name']
-	conn = connect_db()
-	db = conn.cursor()
-	db.execute("SELECT * FROM GAME WHERE NAME1 = ? OR NAME2 = ?", (user, user))
-	conn.commit()
-	conn.close()
-	return "Worked"
+	return "Success"
 
 @app.route("/return_friends", methods = ["POST", "GET"])
 def return_friends():
@@ -76,9 +83,44 @@ def return_friends():
 	conn = connect_db()
 	db = conn.cursor()
 	db.execute("SELECT * FROM FRIENDS WHERE NAME1 = ? OR NAME2 = ?", (user, user))
+	result = db.fetchall()
+	if result is []:
+		return "No Friends."
+	result = [i for sub in result for i in sub if i != user]
 	conn.commit()
 	conn.close()
-	return "Worked"
+	return result
+
+
+@app.route("/make_game", methods = ["POST", "GET"])
+def make_game():
+	incoming = request.json
+	user1 = incoming['name1']
+	user2 = incoming['name2']
+	both_users = [user1, user2]
+	both_users.sort()
+	user1,user2 = both_users[0],both_users[1]
+	conn = connect_db()
+	db = conn.cursor()
+	db.execute("INSERT INTO GAME (NAME1,NAME2,TURN_COUNTER,SCORE1,SCORE2,VIDEO) VALUES (?,?,0,0,0,'Video Link');", (user1, user2))
+	conn.commit()
+	conn.close()
+	return "Success"
+
+
+@app.route("/return_games", methods = ["POST", "GET"])
+def return_games():
+	incoming = request.json
+	user = incoming['name']
+	conn = connect_db()
+	db = conn.cursor()
+	db.execute("SELECT * FROM GAME WHERE NAME1 = ? OR NAME2 = ?", (user, user))
+	result = db.fetchall()
+	if result is []:
+		return "No Games"
+	conn.commit()
+	conn.close()
+	return result
 
 
 if __name__ == '__main__':
--- back-end/curl.sh
+++ back-end/curl.sh
@@ -1,6 +1,12 @@
 #curl -X GET -H "Content-Type: application/json" -d '{"name":"Dan Seminara", "password":"DS"}' http://localhost:6969/create_account;
+#curl -X GET -H "Content-Type: application/json" -d '{"name":"Steven K", "password":"SK"}' http://localhost:6969/create_account;
 #curl -X GET -H "Content-Type: application/json" -d '{"name":"Mitch Kobil", "password":"MK"}' http://localhost:6969/create_account;
 #curl -X GET -H "Content-Type: application/json" -d '{"name":"Kenny Ruddick", "password":"KR"}' http://localhost:6969/create_account;
-#curl -X GET -H "Content-Type: application/json" -d '{"name":"Derek Duchesne", "password":"DD"}' http://localhost:6969/create_account;
+#curl -X GET -H "Content-Type: application/json" -d '{"name":"Derek Dudchesne", "password":"DD"}' http://localhost:6969/create_account;
 #curl -X GET -H "Content-Type: application/json" -d '{"name1":"Derek Duchesne", "name2":"Mitch Kobil"}' http://localhost:6969/make_friends;
-curl -X GET -H "Content-Type: application/json" -d '{"name1":"Mitch Kobil", "name2":"Derek Duchesne"}' http://localhost:6969/make_friends;
+#curl -X GET -H "Content-Type: application/json" -d '{"name1":"Mitch Kobil", "name2":"Derek Duchesne"}' http://localhost:6969/make_friends;
+#curl -X GET -H "Content-Type: application/json" -d '{"name1":"Derek Duchesne", "name2":"Dan Seminara"}' http://localhost:6969/make_friends;
+#curl -X GET -H "Content-Type: application/json" -d '{"name1":"Kenny Ruddick", "name2":"Derek Duchesne"}' http://localhost:6969/make_friends;
+#curl -X GET -H "Content-Type: application/json" -d '{"name":"Derek Duchesne"}' http://localhost:6969/return_friends;
+#curl -X GET -H "Content-Type: application/json" -d '{"name1":"Derek Duchesne", "name2":"Kenny Ruddick"}' http://localhost:6969/make_game;
+curl -X GET -H "Content-Type: application/json" -d '{"name":"Derek Duchesne"}' http://localhost:6969/return_games;
